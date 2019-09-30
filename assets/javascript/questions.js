6444a9fcaf4780801315fc1b5137a3e95088c70f
Eric Hamil <ehamil79@gmail.com>
2019-09-30 18:42:35 -0400

    changed the q&a

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,20 +1,25 @@
 const quizQuestions = [
     {
-        question: "What color is the sky?",
-        choices: ["Brown", "pink", "blue"],
-        correctAnswer: "blue"
+        question: "In Predator, Why Does The Alien Not Attack Unarmed “Prey?",
+        choices: ["He Liked To See Them Run First", "The Predator's Technology Cannot Track People, Just Weapons", "He Confused Them With Jungle Animals", "There’s No Sport In Attacking Unarmed Prey"],
+        correctAnswer: "There’s No Sport In Attacking Unarmed Prey"
     },
 
     {
-        question: "What color is the ground?",
-        choices: ["Brown", "pink", "blue"],
-        correctAnswer: "Brown"
+        question: "What Happens If The Ghostbusters Cross Their Streams?",
+        choices: ["They Turn Into Ghosts", "They Blow Up", "It Reverses The Particle Flow", "The Ghosts Are Tripled In Number"],
+        correctAnswer: "It Reverses The Particle Flow"
     },
 
     {
-        question: "What color are pigs?",
-        choices: ["Brown", "pink", "blue"],
-        correctAnswer: "pink"
+        question: "In Aliens, Who Is The Sole Survivor Of The Terraforming Colony Hadley’s Hope?",
+        choices: ["Gecko", "Salamander", "Newt", "Toad"],
+        correctAnswer: "Newt"
+    },
+    {
+        question: "Before Die Hard, What Was Actor Bruce Willis Primarily Known For?",
+        choices: ["Sci-Fi Television Roles", "Tough Guy Roles", "Comedic Television Roles", "Children's Television"],
+        correctAnswer: "Comedic Television Roles"
     },
 ]; 
 
